8c1e0c6c4893b26f20680b147f778e4040bf4814
calmylake <joaopedrolibonati@gmail.com>
2024-08-24 19:32:33 -0300

    Initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+
+const About = () => {
+  return (
+    <Container>
+      <h1>About This App</h1>
+      <p>This app is built using React, Reactstrap, and React Router. It is deployed on GitHub Pages.</p>
+    </Container>
+  );
+}
+
+export default About;
