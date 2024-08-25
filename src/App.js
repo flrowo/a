0f0574b9cf355f9fd045c3f315cec0dd5eb1b973
calmylake <joaopedrolibonati@gmail.com>
2024-08-25 03:24:58 -0300

    the real first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
+import Main from './pages/Main';
 
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route index path="/" element={<Main />} />
+      </Routes>
     </Router>
   );
 }
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { Collapse, DropdownItem, DropdownMenu, DropdownToggle, Nav, Navbar, NavbarBrand, NavbarText, NavbarToggler, NavItem, NavLink, UncontrolledDropdown } from 'reactstrap';
+
+const NavigationBar = ({ setOpenPage }) => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    let navJson = [
+        {url: "animelist", label: "Anime List"},
+        {url: "socials", label: "Socials/Contact"},
+    ]
+
+    let navList = navJson.map((navObj)=>{
+        return (<>
+            <NavItem key={navObj.url}>
+                <NavLink style={{cursor: 'pointer'}} onClick={() => setOpenPage(navObj.url)}>
+                    {navObj.label}
+                </NavLink>
+            </NavItem>
+        </>);
+    });
+
+    return (
+        <Navbar fixed='top' dark={true} expand={true} style={{backgroundColor: "#25202baa", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)"}}>
+            <NavbarBrand style={{cursor: 'pointer'}} onClick={() => setOpenPage(null)}>flrowo</NavbarBrand>
+            <NavbarToggler onClick={() => setIsOpen(!isOpen)} />
+            <Collapse isOpen={isOpen} navbar>
+                <Nav className="me-auto" navbar>
+
+                    {navList}
+                    
+                    {/* <UncontrolledDropdown nav inNavbar>
+                        <DropdownToggle nav caret>
+                            Options
+                        </DropdownToggle>
+                        <DropdownMenu>
+                            <DropdownItem>Option 1</DropdownItem>
+                            <DropdownItem>Option 2</DropdownItem>
+                            <DropdownItem divider />
+                            <DropdownItem>Reset</DropdownItem>
+                        </DropdownMenu>
+                    </UncontrolledDropdown> */}
+                </Nav>
+                {/* <NavbarText>Simple Text</NavbarText> */}
+            </Collapse>
+        </Navbar>
+    );
+}
+
+export default NavigationBar;
